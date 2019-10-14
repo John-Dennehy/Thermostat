1170c74f516182ef97ae3abfa83b606ef2f57dec
John Dennehy <john.dennehy@gmail.com>
2019-10-14 17:02:37 +0100

    Adds up, down and isMinimumTemp to Thermostat

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,7 +7,27 @@ describe('Thermostat', function() {
     thermostat = new Thermostat(); 
   });
 
-  it ('starts at 20 degrees', function() {
-    expect(thermostat.temp).toEqual(20);
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemp()).toEqual(20);
   });
+
+  it('increases the temp with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemp()).toEqual(21);
+  });
+
+  it('decreases the temp with down()', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemp()).toEqual(19);
+  });
+
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+
+    expect(thermostat.getCurrentTemp()).toEqual(10);
+
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,25 @@
 'use strict';
 
 function Thermostat () {
-  this.temp = 20;
+  this.MINIMUM_TEMP = 10;
+  this._temp = 20;
 }
+
+Thermostat.prototype.getCurrentTemp = function() {
+  return this._temp;
+};
+
+Thermostat.prototype.up = function() {
+  this._temp += 1;
+};
+
+Thermostat.prototype.down = function() {
+  if (this.isMinimumTemp()) {
+    return;
+  }
+  this._temp -= 1;
+};
+
+Thermostat.prototype.isMinimumTemp = function() {
+  return this._temp === this.MINIMUM_TEMP;
+};
